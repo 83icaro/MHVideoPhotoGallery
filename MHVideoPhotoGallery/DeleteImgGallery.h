6febb7573243ba333a3d20ee9eb095f12b9b459b
Danilo Lacerenza <83icaro@MacBook-Pro-di-Danilo-Lacerenza.local>
2015-08-07 19:14:40 +0200

    ok

---
+++ MHVideoPhotoGallery/DeleteImgGallery.h
@@ -0,0 +1,13 @@
+//
+//  DeleteImgGallery.h
+//  MHVideoPhotoGallery
+//
+//  Created by Danilo Lacerenza on 07/08/15.
+//  Copyright (c) 2015 Mario Hahn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeleteImgGallery : NSObject
+
+@end
---
+++ MHVideoPhotoGallery/DeleteImgGallery.m
@@ -0,0 +1,13 @@
+//
+//  DeleteImgGallery.m
+//  MHVideoPhotoGallery
+//
+//  Created by Danilo Lacerenza on 07/08/15.
+//  Copyright (c) 2015 Mario Hahn. All rights reserved.
+//
+
+#import "DeleteImgGallery.h"
+
+@implementation DeleteImgGallery
+
+@end
